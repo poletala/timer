c441fa0d5f565d677ca74bf70f8f1a753885ab39
Alena <92329824+poletala@users.noreply.github.com>
2024-03-04 12:27:42 +0500

    play-pause-stop buttons

---
+++ index.js
@@ -0,0 +1,89 @@
+//timer fields
+const minutesElement = document.querySelector('.minutes');
+const secondsElement = document.querySelector('.seconds');
+
+//buttons
+const playtButton = document.querySelector('.play');
+const newTimertButton = document.querySelector('.newTimer');
+const speakerNonetButton = document.querySelector('.speakerNone');
+
+//listeners
+playtButton.addEventListener('click', () => {
+    clearInterval(interval)
+    onClickStarter()
+
+    // interval = setInterval(startTimer, 1000)
+})
+
+
+//vars
+let minutes = 0,
+    seconds = 0,
+    interval,
+    isOnPlay,
+    isOnPause,
+    isOnStop;
+
+   
+    function startTimer() {
+        isOnPlay = true
+        playtButton.style.backgroundImage = 'url(./assets/ph_pause-circle.svg)'
+        playtButton.style.border = 'none'
+        seconds++
+        if(seconds <= 9) {
+            secondsElement.innerText = '0' + seconds
+        }
+        if(seconds > 9) {
+            secondsElement.innerText = seconds
+        }
+        if(seconds > 59) {
+            minutes++
+            minutesElement.innerText = '0' + minutes
+            seconds = 0
+            secondsElement.innerText = '0' + seconds
+        }
+        if(minutes <= 9) {
+            minutesElement.innerText = '0' + minutes
+        }
+        if(minutes > 9) {
+            minutesElement.innerText = minutes
+        }
+    }
+
+
+function pauseTimer() {
+    playtButton.addEventListener('click', () => {
+        isOnPause = true
+        clearInterval(interval)
+        playtButton.style.backgroundImage = 'url(./assets/ph_stop-circle.svg)'
+    })
+}
+
+function stopTimer() {
+    playtButton.addEventListener('click', () => {
+        isOnStop = true
+        secondsElement.innerText = '00'
+        minutesElement.innerText = '00'
+        playtButton.style.backgroundImage = 'url(./assets/ph_play-circle.svg)'
+    })
+}
+
+    function onClickStarter () {
+        if(isOnPlay) {                    /*работает только при двойном клике*/
+            pauseTimer()
+            isOnPlay = false
+        }
+        if(isOnPause) {                      /*работает только при двойном клике*/
+            stopTimer()
+            isOnPause = false
+        }
+        if(isOnStop) {
+                                            //КАК ЗАПУСТИТЬ ТАЙМЕР ЗАНОВО ПОСЛЕ ОТМЕНЫ?
+        }
+        interval = setInterval(startTimer, 1000)
+    }
+
+
+
+
+
